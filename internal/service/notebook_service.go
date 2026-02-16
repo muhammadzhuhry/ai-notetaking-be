e717605f49c2d52dad600f3fbecdbf9985122fe8
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-16 17:29:22 +0700

    add MoveNotebook method to notebook service for updating notebook parent ID

--- internal/service/notebook_service.go
+++ internal/service/notebook_service.go
@@ -17,6 +17,7 @@ type INotebookService interface {
 	Show(ctx context.Context, id uuid.UUID) (*dto.ShowNotebookResponse, error)
 	Update(ctx context.Context, req *dto.UpdateNotebookRequest) (*dto.UpdateNotebookResponse, error)
 	Delete(ctx context.Context, id uuid.UUID) error
+	MoveNotebook(ctx context.Context, req *dto.MoveNotebookRequest) (*dto.MoveNotebookResponse, error)
 }
 
 type notebookService struct {
@@ -119,4 +120,29 @@ func (c *notebookService) Delete(ctx context.Context, id uuid.UUID) error {
 	}
 
 	return nil
+}
+
+func (c *notebookService) MoveNotebook(ctx context.Context, req *dto.MoveNotebookRequest) (*dto.MoveNotebookResponse, error) {
+	_, err := c.notebookRepository.GetByID(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.ParentId != nil {
+		_, err = c.notebookRepository.GetByID(ctx, *req.ParentId)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = c.notebookRepository.UpdateParentID(ctx, req.Id, req.ParentId)
+	if err != nil {
+		return nil, err
+	}
+
+	res := dto.MoveNotebookResponse{
+		Id: req.Id,
+	}
+
+	return &res, nil
 }
