c352dbaf6a69fcf2e79831db529365fefef63064
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-16 13:08:53 +0700

    add ShowNotebookResponse DTO

--- internal/dto/notebook_dto.go
+++ internal/dto/notebook_dto.go
@@ -1,12 +1,24 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type CreateNotebookRequest struct {
 	Name     string     `json:"name" validate:"required"`
-	ParentID *uuid.UUID `json:"parent_id"`
+	ParentId *uuid.UUID `json:"parent_id"`
 }
 
 type CreateNotebookResponse struct {
 	Id string `json:"id"`
+}
+
+type ShowNotebookResponse struct {
+	Id        string     `json:"id"`
+	Name      string     `json:"name"`
+	ParentId  *uuid.UUID `json:"parent_id,omitempty"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
