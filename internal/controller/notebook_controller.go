b50cccc04c60c914411ef384100619df55f32ce5
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-16 17:29:27 +0700

    add MoveNotebook method to notebook controller for moving notebooks

--- internal/controller/notebook_controller.go
+++ internal/controller/notebook_controller.go
@@ -15,6 +15,7 @@ type INotebookController interface {
 	Show(ctx *fiber.Ctx) error
 	Update(ctx *fiber.Ctx) error
 	Delete(ctx *fiber.Ctx) error
+	MoveNotebook(ctx *fiber.Ctx) error
 }
 
 type notebookController struct {
@@ -31,6 +32,7 @@ func (c *notebookController) RegisterRoutes(r fiber.Router) {
 	h.Get(":id", c.Show)
 	h.Put(":id", c.Update)
 	h.Delete(":id", c.Delete)
+	h.Put(":id/move", c.MoveNotebook)
 }
 
 func (c *notebookController) Create(ctx *fiber.Ctx) error {
@@ -92,4 +94,22 @@ func (c *notebookController) Delete(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.JSON(serverutils.SuccessResponse[any]("Success deleted notebook", nil))
+}
+
+func (c *notebookController) MoveNotebook(ctx *fiber.Ctx) error {
+	idParam := ctx.Params("id")
+	id, _ := uuid.Parse(idParam)
+
+	var req dto.MoveNotebookRequest
+	if err := ctx.BodyParser(&req); err != nil {
+		return err
+	}
+	req.Id = id
+
+	res, err := c.service.MoveNotebook(ctx.Context(), &req)
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(serverutils.SuccessResponse("Success moved notebook", res))
 }
