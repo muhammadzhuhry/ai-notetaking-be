194e3128a336cdc2e0cc9dda45920e1499016286
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-17 16:17:35 +0700

    create note service with create

---
+++ internal/service/note_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"ai-notetaking-be/internal/dto"
+	"ai-notetaking-be/internal/entity"
+	"ai-notetaking-be/internal/repository"
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type INoteService interface {
+	Create(ctx context.Context, req *dto.CreateNoteRequest) (*dto.CreateNoteResponse, error)
+}
+
+type noteService struct {
+	noteRepository repository.INoteRepository
+	// db                 *pgxpool.Pool
+}
+
+func NewNoteService(noteRepository repository.INoteRepository) INoteService {
+	return &noteService{
+		noteRepository: noteRepository,
+	}
+}
+
+func (c *noteService) Create(ctx context.Context, req *dto.CreateNoteRequest) (*dto.CreateNoteResponse, error) {
+	note := entity.Note{
+		Id:         uuid.New(),
+		Title:      req.Title,
+		Content:    req.Content,
+		NotebookId: req.NotebookId,
+		CreatedAt:  time.Now(),
+	}
+
+	err := c.noteRepository.Create(ctx, &note)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.CreateNoteResponse{
+		Id: note.Id,
+	}, nil
+}
