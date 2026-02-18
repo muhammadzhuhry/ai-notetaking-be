8cba4538cb9d87fd63500a5e331c0f78811a341f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-18 13:56:31 +0700

    modify response get all notebook dto

--- internal/dto/notebook_dto.go
+++ internal/dto/notebook_dto.go
@@ -41,10 +41,19 @@ type MoveNotebookResponse struct {
 	Id uuid.UUID `json:"id"`
 }
 
-type GetAllNotebooksResponse struct {
+type GetAllNotebooksResponseNote struct {
 	Id        uuid.UUID  `json:"id"`
-	Name      string     `json:"name"`
-	ParentId  *uuid.UUID `json:"parent_id,omitempty"`
+	Title     string     `json:"title"`
+	Content   string     `json:"content"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
+
+type GetAllNotebooksResponse struct {
+	Id        uuid.UUID                      `json:"id"`
+	Name      string                         `json:"name"`
+	ParentId  *uuid.UUID                     `json:"parent_id,omitempty"`
+	CreatedAt time.Time                      `json:"created_at"`
+	UpdatedAt *time.Time                     `json:"updated_at,omitempty"`
+	Notes     []*GetAllNotebooksResponseNote `json:"notes"`
 }
