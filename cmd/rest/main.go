f2e978b169305851e99528206e03553a80748e1a
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-20 17:12:02 +0700

    inject notebook repository into consumer service

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -47,6 +47,7 @@ func main() {
 		os.Getenv("EMBED_NOTE_CONTENT_TOPIC"),
 		noteRepository,
 		noteEmbeddingRepository,
+		notebookRepository,
 	)
 
 	exampleService := service.NewExampleService(exampleRepository)
