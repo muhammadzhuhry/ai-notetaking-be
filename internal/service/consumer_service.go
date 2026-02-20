bc3bc7d32af96d720774a60b1207c61e5179bbed
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-20 22:05:09 +0700

    adjust get gemini embedding method

--- internal/service/consumer_service.go
+++ internal/service/consumer_service.go
@@ -111,7 +111,7 @@ func (cs *consumerService) processMessage(ctx context.Context, msg *message.Mess
 	)
 
 	// Process embedding
-	res, err := embedding.GetGeminiEmbedding(os.Getenv("GOOGLE_GEMINI_API_KEY"), content)
+	res, err := embedding.GetGeminiEmbedding(os.Getenv("GOOGLE_GEMINI_API_KEY"), content, "RETRIEVAL_DOCUMENT")
 	if err != nil {
 		panic(err)
 	}
