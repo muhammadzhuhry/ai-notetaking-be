1b4caf86693348eda1b8ce2b5c0bd7a3cc2ec81e
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-20 12:29:53 +0700

    move logic to pkg

---
+++ pkg/embedding/gemini_embedding.go
@@ -0,0 +1,86 @@
+package embedding
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type EmbeddingRequestContentPart struct {
+	Text string `json:"text"`
+}
+
+type EmbeddingRequestContent struct {
+	Parts []EmbeddingRequestContentPart `json:"parts"`
+}
+
+type EmbeddingRequest struct {
+	Model    string                  `json:"model"`
+	Content  EmbeddingRequestContent `json:"content"`
+	TaskType string                  `json:"task_type"`
+}
+
+type EmbeddingResponseEmbedding struct {
+	Values []float32 `json:"values"`
+}
+
+type EmbeddingResponse struct {
+	Embedding EmbeddingResponseEmbedding `json:"embedding"`
+}
+
+func GetGeminiEmbedding(apiKey string, text string) (*EmbeddingResponse, error) {
+
+	geminiReq := EmbeddingRequest{
+		Model: "models/gemini-embedding-001",
+		Content: EmbeddingRequestContent{
+			Parts: []EmbeddingRequestContentPart{
+				{
+					Text: text,
+				},
+			},
+		},
+		TaskType: "RETRIEVAL_DOCUMENT",
+	}
+
+	geminiReqJson, err := json.Marshal(geminiReq)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://generativelanguage.googleapis.com/v1beta/models/gemini-embedding-001:embedContent",
+		bytes.NewBuffer(geminiReqJson),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("x-goog-api-key", apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resByte, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error from response, code %d body %s", res.StatusCode, string(resByte))
+	}
+
+	var resEmbedding EmbeddingResponse
+	err = json.Unmarshal(resByte, &resEmbedding)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resEmbedding, nil
+}
