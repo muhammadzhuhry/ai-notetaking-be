f13428fe53911b8c335ae5afe85f9e8dd8c935a3
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-23 14:34:34 +0700

    add get all session an d get chat history dto

--- internal/dto/chatbot_dto.go
+++ internal/dto/chatbot_dto.go
@@ -1,7 +1,25 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type CreateChatSessionResponse struct {
 	Id uuid.UUID `json:"id"`
+}
+
+type GetAllSessionResponse struct {
+	Id        uuid.UUID  `json:"id"`
+	Title     string     `json:"title"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+}
+
+type GetChatHistoryResponse struct {
+	Id        uuid.UUID `json:"id"`
+	Role      string    `json:"role"`
+	Chat      string    `json:"chat"`
+	CreatedAt time.Time `json:"created_at"`
 }
