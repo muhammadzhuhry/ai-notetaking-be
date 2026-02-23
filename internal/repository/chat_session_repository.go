ca3f8a5848db456f4dbd764f1c48b0113f0b0440
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-23 14:33:12 +0700

    add method getall and getbyid

--- internal/repository/chat_session_repository.go
+++ internal/repository/chat_session_repository.go
@@ -5,12 +5,15 @@ import (
 	"ai-notetaking-be/pkg/database"
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type IChatSessionRepository interface {
 	UsingTx(ctx context.Context, tx database.DatabaseQueryer) IChatSessionRepository
 	Create(ctx context.Context, chatSession *entity.ChatSession) error
+	GetAll(ctx context.Context) ([]*entity.ChatSession, error)
+	GetById(ctx context.Context, id uuid.UUID) (*entity.ChatSession, error)
 }
 
 type chatSessionRepository struct {
@@ -44,4 +47,51 @@ func (cs *chatSessionRepository) Create(ctx context.Context, chatSession *entity
 		return err
 	}
 	return nil
+}
+
+func (cs *chatSessionRepository) GetAll(ctx context.Context) ([]*entity.ChatSession, error) {
+	rows, err := cs.db.Query(
+		ctx,
+		`SELECT id, title, created_at, updated_at, deleted_at, is_deleted FROM chat_session WHERE is_deleted = false ORDER BY created_at DESC`,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]*entity.ChatSession, 0)
+	for rows.Next() {
+		var chatSession entity.ChatSession
+		if err := rows.Scan(
+			&chatSession.Id,
+			&chatSession.Title,
+			&chatSession.CreatedAt,
+			&chatSession.UpdatedAt,
+			&chatSession.DeletedAt,
+			&chatSession.IsDeleted,
+		); err != nil {
+			return nil, err
+		}
+		res = append(res, &chatSession)
+	}
+	return res, nil
+}
+
+func (cs *chatSessionRepository) GetById(ctx context.Context, id uuid.UUID) (*entity.ChatSession, error) {
+	row := cs.db.QueryRow(
+		ctx,
+		`SELECT id, title, created_at, updated_at, deleted_at, is_deleted FROM chat_session WHERE id = $1 AND is_deleted = false`,
+		id,
+	)
+	var chatSession entity.ChatSession
+	if err := row.Scan(
+		&chatSession.Id,
+		&chatSession.Title,
+		&chatSession.CreatedAt,
+		&chatSession.UpdatedAt,
+		&chatSession.DeletedAt,
+		&chatSession.IsDeleted,
+	); err != nil {
+		return nil, err
+	}
+	return &chatSession, nil
 }
